bc60b5d3edbcee5122871b64417bde5ee8440123
Aleksandr <aleksandos-777@mail.ru>
2022-06-14 21:35:37 +0300

    added adding new friends

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <header>
     <h1>My Friends</h1>
   </header>
+  <new-friend @add-contact="addNewFriend"></new-friend>
   <section>
     <ul>
       <friend-contact
@@ -32,6 +33,13 @@ export default {
         },
       ],
     };
+  },
+  methods: {
+    addNewFriend(newFriend) {
+      const newFriendId = newFriend.name.toLowerCase();
+      const friendToAdd = { ...newFriend, id: newFriendId };
+      this.friends.push(friendToAdd);
+    },
   },
 };
 </script>
@@ -68,7 +76,8 @@ header {
   list-style: none;
 }
 
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -100,5 +109,19 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
 }
 </style>
---
+++ src/components/NewFriend.vue
@@ -0,0 +1,42 @@
+<template>
+  <form @submit.prevent="submitData">
+    <div>
+      <label for="name">Name</label>
+      <input type="text" name="name" id="name" v-model="name" />
+    </div>
+    <div>
+      <label for="phone">Phone</label>
+      <input type="tel" name="phone" id="phone" v-model="phone" />
+    </div>
+    <div>
+      <label for="email">Email</label>
+      <input type="email" name="email" id="email" v-model="email" />
+    </div>
+    <button @click="addNewFriend">Add New friend</button>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ["add-contact"],
+  data() {
+    return {
+      name: "",
+      phone: "",
+      email: "",
+    };
+  },
+  methods: {
+    submitData() {
+      this.$emit("add-contact", {
+        name: this.name,
+        phone: this.phone,
+        email: this.email,
+      });
+      this.name = "";
+      this.phone = "";
+      this.email = "";
+    },
+  },
+};
+</script>
